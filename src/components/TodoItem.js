2529049a40df07b35edc249ff715234adb0bf870
Raul Barboza <ruzito@gmail.com>
2018-08-28 20:02:17 -0300

    fixed problem with todoItem component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,12 +6,12 @@ import { editTodoBefore, removeTodoBefore } from '../actions/todos';
 class TodoItem extends React.Component {
   constructor(props){
     super(props)
-    
+
     this.handleChange = this.handleChange.bind(this)
     this.handleFocus = this.handleFocus.bind(this)
     this.handleToggle = this.handleToggle.bind(this)
     this.handleRemove = this.handleRemove.bind(this)
-    
+
     this.state = {
       todo: '',
       endDate: props.todoItem.startDate,
@@ -19,24 +19,28 @@ class TodoItem extends React.Component {
       toggleEdit:false
     }
   }
-  
+
   handleToggle(){
     this.setState({toggleEdit:!this.state.toggleEdit})
-    this.props.dispatch(editTodoBefore(this.props.todoItem.id, {todo:this.state.todo}))
+
+    if(this.state.toggleEdit){
+      this.props.dispatch(editTodoBefore(this.props.todoItem.id, {todo:this.state.todo}))
+    }
+
   }
-  
+
   handleRemove(){
     this.setState({endDate: null})
     setTimeout(() => {
       this.props.dispatch(removeTodoBefore(this.props.todoItem.id))
     }, 1000)
-    
+
   }
-  
+
   handleFocus(){
     this.setState({todo:''})
   }
-  
+
   handleChange(event){
     const todo = event.target.value ? event.target.value : '';
     this.setState({todo:todo})
@@ -44,20 +48,20 @@ class TodoItem extends React.Component {
   }
 
   componentDidMount(props, state){
-    
+
     this.setState({todo: this.props.todoItem.todo})
     let task = this.state.endDate;
-    
+
     let timeUpdate = setInterval(() => {
-      
+
       let now = moment();
       let difference = now.diff(task, 'minutes')
       difference = difference*-1
-      
-  
+
+
       let minutes = difference % 60;
       let hours = (difference - minutes) / 60;
-  
+
       if(hours > 0 && hours < 24){
         this.setState({tempoRestante: hours+'h'})
       }else if(hours > 23){
@@ -71,11 +75,11 @@ class TodoItem extends React.Component {
       }
       if(!this.state.endDate){clearInterval(timeUpdate)}
     },1000)
-    
+
     timeUpdate
-    
+
   }
-  
+
   render(){
     return(
       <li className={"list-group-item " + (this.state.tempoRestante ? '' : 'list-group-item-danger')}>
@@ -83,7 +87,7 @@ class TodoItem extends React.Component {
           <span onClick={this.handleRemove} className="glyphicon glyphicon-remove pull-right" aria-hidden="true"></span>
           <span onClick={this.handleToggle} className={"glyphicon pull-left " + (this.state.toggleEdit ? 'glyphicon-ok' : 'glyphicon-pencil')} aria-hidden="true"></span>
         </div>
-        {this.state.toggleEdit ? <input className="inputEdit" onFocus={this.handleFocus} value={this.state.todo} onChange={this.handleChange} type="text"/> : <h4>{this.props.todoItem.todo}</h4>}
+        {this.state.toggleEdit ? <input className="inputEdit" onFocus={this.handleFocus} value={this.state.todo} onChange={this.handleChange} type="text"/> : <h4>{this.state.todo}</h4>}
         <p>{this.state.tempoRestante}</p>
       </li>
     )
